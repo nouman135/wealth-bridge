ed8f1136cd2425ff3c3df8d38f20e33593c4ad99
Sheila Chebet <chebetsheila170@gmail.com>
2023-03-18 20:23:15 +0000

    Side nav added

--- src/components/RMPage.js
+++ src/components/RMPage.js
@@ -1,10 +1,18 @@
 import React from "react";
-
+import "../components/RMpage.css";
 
 export default function RMPage() {
   return (
     <div>
-      <h1>This is the RM page</h1>
+      <h1>RM Dashboard</h1>
+      <div class="sidenav">
+        <li><a href="#about">Profile</a></li>
+        <li><a href="#services">Category</a></li>
+        <li><a href="#clients">Clients</a></li>
+        <li><a href="#contact">Logout</a></li>
+      </div>
+      <p>Inviestment ideas</p>
+      
     </div>
   );
 }
