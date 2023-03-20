30113cd101b8970857b29407af1a96dcebe4632e
Nouman Ali malik <noumanmalik970@gmail.com>
2023-03-20 23:27:04 +0500

    resolved formating issue

--- src/components/outernavbar.component.js
+++ src/components/outernavbar.component.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Outernavbar extends Component {
   render() {
     return (
       <div>
-         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-in'}>
+                  <Link className="nav-link" to={"/sign-in"}>
                     Login
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
+                  <Link className="nav-link" to={"/sign-up"}>
                     Sign up
                   </Link>
                 </li>
@@ -25,6 +24,6 @@ export default class Outernavbar extends Component {
           </div>
         </nav>
       </div>
-    )
+    );
   }
 }
