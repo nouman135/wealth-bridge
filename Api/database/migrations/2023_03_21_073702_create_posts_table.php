6713ad139d270a7626b128f70beaf4c9a2236d13
Nouman Ali malik <noumanmalik970@gmail.com>
2023-03-21 13:20:42 +0500

    updated attributes in post db

--- Api/database/migrations/2023_03_21_073702_create_posts_table.php
+++ Api/database/migrations/2023_03_21_073702_create_posts_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('detail');
             $table->timestamps();
         });
     }
