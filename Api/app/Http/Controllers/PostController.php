68557f3756b02f7a04e1eb03a756ce0944ab135d
Nouman Ali malik <noumanmalik970@gmail.com>
2023-03-23 20:04:51 +0500

    function for get all api

--- Api/app/Http/Controllers/PostController.php
+++ Api/app/Http/Controllers/PostController.php
@@ -7,5 +7,17 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json([
+            'status' => 'success',
+            'posts' => $posts,
+        ]);
+    }
 }
