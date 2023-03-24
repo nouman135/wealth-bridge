04d3141d95e191be36980b364b605f266efe3d16
Nouman Ali malik <noumanmalik970@gmail.com>
2023-03-24 14:48:15 +0500

    get all post service

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -2,6 +2,18 @@ import { axiosInstance, getAuthorizationHeader } from "../helper/http-common";
 
 class PostService {
 
+  async getPosts() {
+    try {
+      const response = await axiosInstance.get("/posts", {
+        headers: { Authorization: getAuthorizationHeader() },
+      });
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
 }
 
 export default new PostService()
