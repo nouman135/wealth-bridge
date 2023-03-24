4f7f9082ca0234a116c8c4c6b3c86b8bc59f0e8e
Nouman Ali malik <noumanmalik970@gmail.com>
2023-03-25 00:02:39 +0500

    post servies updated

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,7 +1,6 @@
 import { axiosInstance, getAuthorizationHeader } from "../helper/http-common";
 
-class PostService {
-
+export default new (class PostService {
   async getPosts() {
     try {
       const response = await axiosInstance.get("/posts", {
@@ -14,6 +13,51 @@ class PostService {
     }
   }
 
-}
+  async getById(postId) {
+    try {
+      const response = await axiosInstance.get("/post/" + postId, {
+        headers: { Authorization: getAuthorizationHeader() },
+      });
 
-export default new PostService()
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async createPost(post) {
+    try {
+      const response = await axiosInstance.post("/post", post, {
+        headers: { Authorization: getAuthorizationHeader() },
+      });
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updatePost(post, postId) {
+    try {
+      const response = await axiosInstance.put("/post/" + postId, post, {
+        headers: { Authorization: getAuthorizationHeader() },
+      });
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deletePost(postId) {
+    try {
+      const response = await axiosInstance.delete("/post/" + postId, {
+        headers: { Authorization: getAuthorizationHeader() },
+      });
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+})();
