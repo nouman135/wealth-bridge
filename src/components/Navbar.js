439b2c0a47d8aa5fcf126d8a6f0a2ebb5ab2090b
Nouman Ali malik <noumanmalik970@gmail.com>
2023-03-27 03:20:11 +0500

    navbar funcationality change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,86 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import HomePage from './post/HomePage'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import HomePage from "./post/HomePage";
 
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    const udata = localStorage.getItem("user");
+    const odata = JSON.parse(udata);
+    let loggedIN = true;
+    if (udata == null) {
+      loggedIN = false;
+    }
+    this.state = {
+      user: odata?.user,
+      loggedIN,
+    };
+  }
 
-export default function Navbar() {
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand ml-3" href="#">
-            Wealth Bridge
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to='./'>
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to='./dashboard'>
-                  Dashboard
-                </Link>
-              </li>
-            </ul>
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Login
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Sign up
-                </a>
-              </li>
-            </ul>
+  render() {
+    const isLoggedIn = this.state.loggedIN;
+    let button;
+    let button2;
+    if (isLoggedIn) {
+      button = (
+        <Link className="nav-link" to="./logout">
+          {" "}
+          logout{" "}
+        </Link>
+      );
+    } else {
+      button = (
+        <Link className="nav-link" to="./sign-in">
+          {" "}
+          login{" "}
+        </Link>
+      );
+      button2 = (
+        <Link className="nav-link" to="./sign-up">
+          {" "}
+          Sign up{" "}
+        </Link>
+      );
+    }
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container">
+            <a className="navbar-brand ml-3" href="#">
+              Wealth Bridge
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link" to="./">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="./dashboard">
+                    Dashboard
+                  </Link>
+                </li>
+              </ul>
+              <ul className="navbar-nav">
+                <li className="nav-item">{button}</li>
+                <li className="nav-item">{button2}</li>
+              </ul>
+            </div>
           </div>
         </nav>
-    </div>
-  )
+      </div>
+    );
+  }
 }
