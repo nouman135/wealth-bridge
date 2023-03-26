7bc47378719a3fc22c78632f5e182913c58c7d24
Nouman Ali malik <noumanmalik970@gmail.com>
2023-03-27 03:18:50 +0500

    removed unnessary code

--- src/components/dashboard.component.js
+++ src/components/dashboard.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Navigate } from "react-router-dom";
 import List from "./post/list.component";
+
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
@@ -22,53 +23,6 @@ export default class Dashboard extends Component {
     }
     return (
       <div>
-        {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/dashboard">
-              Wealth Bridge
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a
-                    className="nav-link active"
-                    aria-current="page"
-                    href="/dashboard"
-                  >
-                    Home
-                  </a>
-                  <a
-                    className="nav-link active"
-                    aria-current="page"
-                    href="/dashboard"
-                  >
-                    Home
-                  </a>
-                </li>
-              </ul>
-              <ul className="navbar-nav">
-                <a className="nav-link" href="/logout">
-                  logout{" "}
-                </a>
-              </ul>
-            </div>
-          </div>
-        </nav> */}
-
         <h1 className="text-black mt-5">
           welcome to your profile{" "}
           <span className="text-primary">{this.state.user.first_name} </span>
