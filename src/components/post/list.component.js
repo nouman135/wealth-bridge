77244ed1ec1bddddc6218a4002b98d45bcd83266
Nouman Ali malik <noumanmalik970@gmail.com>
2023-03-29 01:15:04 +0500

    list component design change

--- src/components/post/list.component.js
+++ src/components/post/list.component.js
@@ -1,85 +1,94 @@
 import React, { Component } from "react";
 import PostService from "../../services/post.service";
 import withRouter from "../../helper/HOC";
-import { convertISODateTime } from "../../helper/date"
+import { convertISODateTime } from "../../helper/date";
 import "./list.css";
 
-export default withRouter (class List extends Component {
-  constructor(props) {
-    super(props);
+export default withRouter(
+  class List extends Component {
+    constructor(props) {
+      super(props);
 
-    this.state = {
-      posts: [],
-    };
+      this.state = {
+        posts: [],
+      };
 
-    this.addPost = this.addPost.bind(this);
-    this.editPost = this.editPost.bind(this);
-    this.deletePost = this.deletePost.bind(this);
-  }
+      this.addPost = this.addPost.bind(this);
+      this.editPost = this.editPost.bind(this);
+      this.deletePost = this.deletePost.bind(this);
+    }
 
-  deletePost(id) {
-    PostService.deletePost(id).then((res) => {
-      this.setState({
-        posts: this.state.posts.filter((post) => post.id !== id),
+    deletePost(id) {
+      PostService.deletePost(id).then((res) => {
+        this.setState({
+          posts: this.state.posts.filter((post) => post.id !== id),
+        });
       });
-    });
-  }
+    }
 
-  editPost(id) {
-    const { navigate } = this.props;
-    navigate(`/add-post/${id}`);
-  }
+    editPost(id) {
+      const { navigate } = this.props;
+      navigate(`/add-post/${id}`);
+    }
 
-  componentDidMount() {
-    PostService.getPosts().then((res) => {
-      this.setState({ posts: res.data.posts });
-    });
-  }
+    componentDidMount() {
+      PostService.getPosts().then((res) => {
+        this.setState({ posts: res.data.posts });
+      });
+    }
 
-  addPost() {
-    const { navigate } = this.props;
-    navigate(`/add-post/_add`);
-  }
+    addPost() {
+      const { navigate } = this.props;
+      navigate(`/add-post/_add`);
+    }
 
-  render() {
-    return (
-      <div>
-        <header>
-          <h2>Dashboard</h2>
-          <div className="header-buttons">
-            <button>Add RM</button>
+    render() {
+      return (
+        <div className="container-fluid">
+          <header>
+            <h2>Dashboard</h2>
+            <div className="header-buttons">
+              <button>Add RM</button>
               <button onClick={() => this.addPost()}>Add New Idea</button>
-          </div>
-        </header>
-        <main>
-          {this.state.posts.map((post) => (
-            <ul className="idea-list" key={post.id}>
-              <li className="idea-item">
-                <div className="idea-title">Title: {post.title}</div>
-                <div className="idea-category">Category: {post.category}</div>
-                <div className="idea-description">
-                  Description: {post.description}
-                </div>
-                <div className="idea-date">
-                  Created At: {convertISODateTime(post.created_at)}
-                </div>
-                <div className="idea-date">
-                  Updated At: {convertISODateTime(post.updated_at)}
-                </div>
-                <div className="idea-actions">
-                    <button onClick={() => this.editPost(post.id)}>
-                      Update{" "}
+            </div>
+          </header>
+          <main className="row">
+            {this.state.posts.map((post) => (
+              <div
+                key={post.id}
+                className="col-6 my-2 "
+              >
+                <div className="card shadow-sm text-center h-100" key={post.id}>
+                  <div className="card-header">
+                    <h3>{post.title}</h3>
+                    <p className="card-title">{post.category}</p>
+                  </div>
+                  <div className="card-body">
+                    <p className="card-text">{post.description}</p>
+                    <p className="card-text">
+                      {convertISODateTime(post.updated_at)}
+                    </p>
+                  </div>
+                  <div className="card-footer">
+                    <button
+                      className="btn btn-sm"
+                      onClick={() => this.editPost(post.id)}
+                    >
+                      Update
                     </button>
-                  <button onClick={() => this.deletePost(post.id)}>
-                    Delete{" "}
-                  </button>
-                  <button>Share</button>
+                    <button
+                      className="btn btn-danger btn-sm"
+                      onClick={() => this.deletePost(post.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
                 </div>
-              </li>
-            </ul>
-          ))}
-        </main>
-      </div>
-    );
+              </div>
+            ))}
+          </main>
+        </div>
+      );
+    }
   }
-})
+);
