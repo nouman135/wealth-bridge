c81679ee56a5f369be5e0039443d9068d6ec5491
nouman malik <noumanmalik970@gmail.com>
2023-04-04 15:35:08 +0500

    pretify and improvement

--- src/components/dashboard.component.js
+++ src/components/dashboard.component.js
@@ -1,59 +1,61 @@
 import React, { Component } from "react";
 import { Navigate } from "react-router-dom";
-import List from "./post/list.component";
+import PostList from "./post/list.component";
 import { getUser } from "../helper/http-common";
 import Navbar from "./navbar.component";
 import withRouter from "../helper/HOC";
 import "./dashboard.component.css";
 
-export default withRouter(class Dashboard extends Component {
-  constructor(props) {
-    super(props);
+export default withRouter(
+  class Dashboard extends Component {
+    constructor(props) {
+      super(props);
 
-    const udata = getUser();
-    let loggedIN = true;
+      const udata = getUser();
+      let loggedIN = true;
 
-    if (udata == null) {
-      loggedIN = false;
+      if (udata == null) {
+        loggedIN = false;
+      }
+
+      this.state = {
+        user: udata?.user,
+        loggedIN,
+      };
+
+      this.addPost = this.addPost.bind(this);
+      this.addMember = this.addMember.bind(this);
+
+      this.navigate = this.props.navigate;
     }
 
-    this.state = {
-      user: udata?.user,
-      loggedIN,
-    };
-
-    this.addPost = this.addPost.bind(this);
-    this.addMember = this.addMember.bind(this);
-
-    this.navigate = this.props.navigate;
-  }
-
-  addPost() {
-    this.navigate(`/add-post/_add`);
-  }
-
-  addMember() {
-    this.navigate(`/add-member`);
-  }
-
-  render() {
-    if (this.state.loggedIN === false) {
-      return <Navigate to="/sign-in" />;
+    addPost() {
+      this.navigate(`/add-post/_add`);
     }
-    return (
-      <>
-        <Navbar />
-        <div className="container mt-5">
-          <header className="">
-            <h2>Dashboard</h2>
-            <div className="header-buttons">
-              <button onClick={() => this.addMember()}>Add RM</button>
-              <button onClick={() => this.addPost()}>Add New Idea</button>
-            </div>
-          </header>
-        </div>
-        <List />
-      </>
-    );
+
+    addMember() {
+      this.navigate(`/add-member`);
+    }
+
+    render() {
+      if (this.state.loggedIN === false) {
+        return <Navigate to="/sign-in" />;
+      }
+      return (
+        <>
+          <Navbar />
+          <div className="container mt-5">
+            <header className="">
+              <h2>Dashboard</h2>
+              <div className="header-buttons">
+                <button onClick={() => this.addMember()}>Add RM</button>
+                <button onClick={() => this.addPost()}>Add New Idea</button>
+              </div>
+            </header>
+            <PostList />
+          </div>
+        </>
+      );
+    }
   }
-})
+);
